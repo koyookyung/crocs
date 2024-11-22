cccc5013301b74a6eea56a77f0248578f86794e9
koyookyung <rrrrr514@naver.com>
2024-11-22 14:28:28 +0900

    Add files via upload

--- javascript/script.js
+++ javascript/script.js
@@ -1,29 +1,68 @@
 jQuery(document).ready(function () {
 
-    //2차메뉴
-    $('.gnb>li').mouseover(function (){
+    //2차메뉴 - pc
+    $('.menu_pc .gnb>li').mouseover(function () {
+        $('.depth02').stop().slideUp(500);
         $(this).find('.depth02').stop().slideDown(500);
         $('#menu_bg').stop().slideDown(500);
-    }).mouseout(function () {
-        $(this).find('.depth02').stop().slideUp(500);
+    }); 
+    $('.menu_pc').mouseleave(function () {
+        $('.depth02').stop().slideUp(500);
         $('#menu_bg').stop().slideUp(500);
     });
-    $('.gnb>li:first-child').mouseover(function () {
-        $('#menu_bg').stop();
+   
+    $('.menu_pc .gnb>li:first-child').mouseover(function () {
+        $('#menu_bg').stop().slideUp();
     });
-    $('.gnb>li:last-child').mouseover(function () {
-        $('#menu_bg').stop();
+    $('.menu_pc .gnb>li:last-child').mouseover(function () {
+        $('#menu_bg').stop().slideUp();
     });
 
-    window.onload = () => { // 실행 코드: () =>
-        let menuBtn = document.querySelector('.menu_btn ul li:first-child')
-        //  let: 변수 선언 (코드가 짧아지도록 이름 변경)
-        let mobileNav = document.querySelector('.nav_mobile')
+    //2차메뉴 - mobile
+    $('.nav_mobile .gnb > li:nth-child(n+2):nth-child(-n+7)').click (function () {
+        $(this).siblings('li').toggleClass('on')
+        $('.gnb') .toggleClass('on');
+        /* $('.nav_mobile .depth01').css({'text-align': 'center'}); */
+    });
 
-        //menuBtn 클릭했을 때 이벤트 실행
-        menuBtn.addEventListener('click', () => {
-            //mobile_nav에게 클래스명 active 추가&삭제
-            mobileNav.classList.toggle('active'); // classList: 클래스 제어
+});//jQuery end
+
+window.onload = () => {
+
+    // 2차메뉴 - mobile
+    let menuBtn = document.querySelector('.menu_btn ul li:first-child');
+    let mobileNav = document.querySelector('.nav_mobile');
+    let body = document.querySelector('body');
+
+    menuBtn.addEventListener('click', () => {
+        mobileNav.classList.toggle('active');
+        body.classList.toggle('on');
+    });
+
+    //탭메뉴 - 지비츠
+    let tabMenu = document.querySelectorAll('.jibbitz .tag ul li');
+    let itemWrap = document.querySelectorAll('.jibbitz .pc_item_wrap');
+    let itemWrapMobile = document.querySelectorAll('.jibbitz .mobile_item_wrap');
+
+    for(let i = 0; i < tabMenu.length; i++) {
+
+        tabMenu[i].addEventListener('click', () => {
+
+            tabMenu.forEach(menuItem => {
+                menuItem.classList.remove('active');
+            })
+            tabMenu[i].classList.add('active');
+
+            itemWrap.forEach(item => {
+                item.classList.remove('active');
+            })
+            itemWrap[i].classList.add('active');
+
+            itemWrapMobile.forEach(item => {
+                item.classList.remove('active');
+            })
+            itemWrapMobile[i].classList.add('active');
         });
-    } //script end
-});
+    }
+    
+};//script end
